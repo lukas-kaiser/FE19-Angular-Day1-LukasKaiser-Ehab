b2452e7511e5636ea8603792d503bd181233ab87
Lukas Kaiser <lukas.kaiser@kununu.com>
2023-06-19 22:05:32 +0200

    add some cards

--- Day1/src/app/content/content.component.ts
+++ Day1/src/app/content/content.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
-
+location1 : string = "Kitzbühel";
+location1Text : string = "Kitzbühel is a small Alpine town east of Innsbruck, in the western Austrian province of Tyrol. It's a fashionable winter resort, known for the annual Hahnenkamm downhill race.";
+location1Image : string = "/assets/kitzbuhel.jpg";
+location2 : string = "Schladming";
+location2Text : string = "Schladming is a small former mining town in the northwest of the Austrian state of Styria that is now a popular tourist destination.";
+location2Image : string = "/assets/schladming.jpg";
 }
+
+// let destinations: {name : string; image : string, text : string}[] = [{
+// name: "Kitzbühel",
+// image: "assets/kitzbühel.jpg",
+// text: "Kitzbühel is a small Alpine town east of Innsbruck, in the western Austrian province of Tyrol. It's a fashionable winter resort, known for the annual Hahnenkamm downhill race."
+// },{
+//   name: "Schladming",
+// image: "assets/schladming.jpg",
+// text: "Schladming is a small former mining town in the northwest of the Austrian state of Styria that is now a popular tourist destination."
+// }]
